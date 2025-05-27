f16683f83a5dd3d5119b6658d6ee4d4e668f38d5
Marvin Müller <marvin.mueller.fmb@gmail.com>
2025-05-27 17:05:47 +0200

    Changed topo

--- src/topologies/BasicTopologie.py
+++ src/topologies/BasicTopologie.py
@@ -1,9 +1,10 @@
 from mininet.topo import Topo
 from mininet.node import OVSKernelSwitch
+from mininet.link import TCLink
 
 class SimulationTopo(Topo):
     def __init__(self, queue_size=100, **opts):
-        self.queue_size = queue_size
+        #self.queue_size = queue_size
         super(SimulationTopo, self).__init__(**opts)
 
     def build(self):
@@ -18,8 +19,8 @@ class SimulationTopo(Topo):
         r2 = self.addSwitch('r2', cls=OVSKernelSwitch)
         
         # Links
-        self.addLink(s1, r1, bw=10, delay='2ms')
-        self.addLink(s2, r1, bw=10, delay='3ms')
-        self.addLink(r1, r2, bw=1.5, delay='3ms')
-        self.addLink(r2, s3, bw=10, delay='10ms')
-        self.addLink(r2, s4, bw=0.5, delay='5ms', max_queue_size=self.queue_size)
+        self.addLink(s1, r1)
+        self.addLink(s2, r1)
+        self.addLink(r1, r2, cls=TCLink, bw=10, delay='0.1ms')
+        self.addLink(r2, s3)
+        self.addLink(r2, s4)
